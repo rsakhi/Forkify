861d89267ad02fa45db29986aef2aae2633413d7
rsakhi <sakhi.rohit@gmail.com>
2019-02-06 18:30:37 +0530

    Created search contoller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
-import {add} from './views/searchView'
-import Search from './models/Search'
+// import {add} from './views/searchView'
+// console.log(add(2,3));
 
-const search = new Search("pizza");
-search.getSearchResult();
-console.log(add(2,3));
+import Search from './models/Search';
+
+
+// const search = new Search("pizza");
+// search.getSearchResult();
+
+const state = {};
+
+const controleSearch = async () => {
+
+    //1) read input from UI
+    const quary = "pizza";
+    //2) create new Search Object
+    state.search = new Search(quary);
+    // ) prepare UI for search result
+
+    //3) make API call
+    await state.search.getSearchResult();
+    //4) display result 
+    console.log("from index");
+    console.log(state.search.results);
+}
+
+document.querySelector('.search__btn').addEventListener('click', e => {
+    e.preventDefault();
+    console.log("fro");
+    controleSearch();
+})
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,9 +7,11 @@ export default class Search {
 
     async getSearchResult(){
         const proxy = "https://cors-anywhere.herokuapp.com";
-        const key = "f82ff1015c6efa9f79ed6b46c8137266";
+        // const key = "f82ff1015c6efa9f79ed6b46c8137266";
+        const key = "b7ff75ba073ef4c01820e663878c2e8c";
         try{
             const res = await axios(`${proxy}/https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            console.log(this.results);
             this.results = res.data.recipes;
             console.log(this.results);
         }catch (error){
