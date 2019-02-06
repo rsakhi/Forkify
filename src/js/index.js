6d76fdb44bbc2482419f2b21dd25f6c4344a4105
rsakhi <sakhi.rohit@gmail.com>
2019-02-06 19:09:57 +0530

    display All recepies on UI

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@
 // console.log(add(2,3));
 
 import Search from './models/Search';
-
+import * as searchView from './views/searchView'
+import {elements} from "./views/base";
 
 // const search = new Search("pizza");
 // search.getSearchResult();
@@ -12,20 +13,22 @@ const state = {};
 const controleSearch = async () => {
 
     //1) read input from UI
-    const quary = "pizza";
+    const quary = searchView.getInput();
+    console.log(quary);
+
+
     //2) create new Search Object
     state.search = new Search(quary);
     // ) prepare UI for search result
 
     //3) make API call
     await state.search.getSearchResult();
-    //4) display result 
-    console.log("from index");
+    //4) display result
+    searchView.renderResults(state.search.results);
     console.log(state.search.results);
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
-    console.log("fro");
     controleSearch();
 })
